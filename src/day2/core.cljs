bab1919c51be9839d31bde47039a9570ab78377f
Ed Jeffreys <ed.jeffreys@starlingbank.com>
2021-12-02 16:53:43 +0000

    day 2 part 2

--- src/day2/core.cljs
+++ src/day2/core.cljs
@@ -3,17 +3,16 @@
   (:require [clojure.java.io :as io]))
 
 (defn move
-  [[x, y], direction, amount] (cond
-                              (= direction "forward") [(+ x amount), y]
-                              (= direction "up") [x, (- y amount)]
-                              (= direction "down") [x, (+ y amount)]))
+  [[x, y, aim], direction, amount] (cond
+                              (= direction "forward") [(+ x amount), (+ y (* aim amount)), aim]
+                              (= direction "up") [x, y, (- aim amount)]
+                              (= direction "down") [x, y, (+ aim amount)]))
 
 (defn follow_route
-  ([input] (follow_route input [0, 0] 0))
-  ([input, [x, y], i] (if (= i (count input)) [x, y]
-                             (recur input (move [x, y] (nth (str/split (nth input i) #" ") 0) (Integer/parseInt (nth (str/split (nth input i) #" ") 1))) (inc i)))))
+  ([input] (follow_route input [0, 0, 0] 0))
+  ([input, [x, y, aim], i] (if (= i (count input)) [x, y]
+                             (recur input (move [x, y, aim] (nth (str/split (nth input i) #" ") 0) (Integer/parseInt (nth (str/split (nth input i) #" ") 1))) (inc i)))))
 
 (def input (str/split (slurp (io/resource "day2/input.txt")) #"\n"))
 
 (println (reduce * (follow_route input)))
-
